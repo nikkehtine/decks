0b4a886d5766d2d6f31b1dc19a884690f23ec6a2
nikkehtine <27138416+nikkehtine@users.noreply.github.com>
2026-02-22 04:00:20 +0100

    Slideshow Viewer small styling change

--- src/components/slideshow/Viewer.tsx
+++ src/components/slideshow/Viewer.tsx
@@ -5,7 +5,6 @@ export const Viewer = () => (
       width: "min(100vw, calc(100vh * 4 / 3))",
       height: "min(100vh, calc(100vw * 3 / 4))",
     }}
-    // TODO: this CSS style was vibecoded, check out better approach later
   >
     <span>Lorem ipsum</span>
   </article>
--- src/pages/SlideshowViewer.tsx
+++ src/pages/SlideshowViewer.tsx
@@ -1,7 +1,7 @@
 import { Viewer } from "@/components/slideshow/Viewer";
 
 export const SlideshowViewer = () => (
-  <main className="grid min-h-dvh place-items-center bg-white dark:bg-black">
+  <main className="fixed inset-0 grid place-items-center bg-white dark:bg-black">
     <Viewer />
   </main>
 );
